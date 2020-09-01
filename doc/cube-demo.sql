134aa8057ac941f01b44be9b72bd6e6fac99d4da
yaochenglong <289911401@qq.com>
2020-09-01 17:23:51 +0800

    spring 事务demo

---
+++ doc/cube-demo.sql
@@ -0,0 +1,53 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.22-log : Database - cube-demo
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`cube-demo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `cube-demo`;
+
+/*Table structure for table `sys_balance` */
+
+DROP TABLE IF EXISTS `sys_balance`;
+
+CREATE TABLE `sys_balance` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) DEFAULT NULL,
+  `balance` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_balance` */
+
+insert  into `sys_balance`(`id`,`user_name`,`balance`) values (1,'zhangsan','60.00'),(2,'lisi','40.00');
+
+/*Table structure for table `sys_log` */
+
+DROP TABLE IF EXISTS `sys_log`;
+
+CREATE TABLE `sys_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) DEFAULT NULL,
+  `operate` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_log` */
+
+insert  into `sys_log`(`id`,`user_name`,`operate`,`create_time`) values (1,'zhangsan','转账','2020-09-01 11:50:45'),(2,'zhangsan','转账','2020-09-01 16:30:16');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
